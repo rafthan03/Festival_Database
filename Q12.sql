b7eef1e551b7089a5d5f30014414263636a3007b
rafthan03 <117581474+rafthan03@users.noreply.github.com>
2025-10-04 18:52:41 +0300

    Delete old SQL files

--- Q12.sql
+++
@@ -1,7 +0,0 @@
-/*Βρείτε το προσωπικό που απαιτείται για κάθε ημέρα του φεστιβάλ, παρέχοντας ανάλυση ανά κατηγορία (τεχνικό
-προσωπικό ασφαλείας, βοηθητικό προσωπικό)*/
-Select sum(CEIL(s.max_capacity*0.02)) as Βοηθητικό, sum(CEIL(s.max_capacity*0.05)) as Ασφάλεια,sum(CEIL(s.max_capacity*0.01)) as Τεχνικό, f.festival_id, e.event_date as ημέρα
-FROM stage s
-JOIN event e ON s.stage_id=e.stage_id
-JOIN festival f ON f.festival_id=e.festival_id
-Group by e.event_date;
